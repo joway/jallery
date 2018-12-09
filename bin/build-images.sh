e6fff1128e59315335018d52cdf3ca6d82f96675
Joway Wang <joway.w@gmail.com>
2018-12-09 17:01:05 +0800

    add build photos

--- bin/build-images.sh
+++ bin/build-images.sh
@@ -1,14 +1,15 @@
 # /bin/bash
-find ./public/photos -mindepth 2 -maxdepth 2 -type f -name '*.png' -o -name '*.jpg' -exec sh -c '
+find ./public/raw -mindepth 2 -maxdepth 2 -type f -name '*.png' -o -name '*.jpg' -exec sh -c '
   for f do
-    echo "add watermark: $f"
     source=$f
-    # convert -resize 2073600@ $source $source
-    # width=`identify -format %w $source`
-    # height=`identify -format %h $source`
-    # convert -strip -interlace Plane -quality 70% -background 'none'  -fill white -gravity center \
-    #   -size $(expr ${width} / 10)x$(expr ${height} / 36) \
-    #   caption:"@Joway Wang" \
-    #   $source +swap -gravity south -geometry +0+$(expr ${height} / 100) -composite $source
+    output=${source/raw/photos}
+    echo "add watermark: from $source to $output"
+    convert -resize 2073600@ $source $output
+    width=`identify -format %w $output`
+    height=`identify -format %h $output`
+    convert -strip -interlace Plane -quality 70% -background 'none'  -fill white -gravity center \
+      -size $(expr ${width} / 10)x$(expr ${height} / 36) \
+      caption:"@Joway Wang" \
+      "$output" +swap -gravity south -geometry +0+$(expr ${height} / 100) -composite "$output"
   done
 ' sh {} +
