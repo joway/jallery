0fd3df1d167f779ce00938ee81c454fc42259e67
Joway Wang <joway.w@gmail.com>
2019-01-11 01:25:13 +0800

    add watermark

--- bin/imagic.sh
+++ bin/imagic.sh
@@ -6,6 +6,10 @@ find ./public/raw -mindepth 1 -maxdepth 1 -type d -exec sh -c '
     source=$f
     output=`echo $source | sed "s/raw/photos/g"`
     imagic compress -p 10 -q 70 -o $output  $source/*
+    imagic resize -w 2000 -p 10  $output/*
+    imagic watermark -x -1100 -y -100 \
+      -t ./public/texture.png \
+      -p 10 $output/*
     imagic resize -w 480 -p 10 -s .thumbnail  $output/*
   done
 ' sh {} +
